2919d04a3a3cd8f47f3376262420a2ecddcbd09d
C33J4Y <cherrera2018@fau.edu>
2021-02-07 22:53:34 +0000

    Added comments.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 using namespace std;
 
-
+//Program to find if a string is palindrome
+//Iterates through half of the string and checks is the first
+//Half of the string and checks if it equals the second half.
+//as soon as a character from the first half does not match the 
+//second half. it returns FALSE.
 
 int main() {
   string s = "tacocaat";
